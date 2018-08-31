b63480b8ab13aae154bf43e79c98b73340ee2cee
Iulian Rotaru <iulian@gnotis.ro>
2018-08-31 22:11:34 +0300

    Initial import.

---
+++ Assets/Main Menu/Scripts/ExitButtonHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButtonHandler : MonoBehaviour {
+    public void OnExitButton()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
---
+++ Assets/StartButtonHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButtonHandler : MonoBehaviour {
+    public void OnStartButton(int sceneBuildIndex)
+    {
+        SceneManager.LoadScene(sceneBuildIndex);
+    }
+}
